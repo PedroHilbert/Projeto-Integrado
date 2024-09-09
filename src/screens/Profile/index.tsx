fde17c538afcc2dffb868bce0fdf496117b4bbdd
abnercoolman <abnercoolman@gmail.com>
2024-09-08 21:54:39 -0300

    feat: Update Perfil

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,6 +1,8 @@
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, ScrollView, Image } from "react-native";
 import { styles } from "./styles";
 import ScreenHeader from "../../components/ScreenHeader";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export default function Profile() {
   return (
@@ -9,7 +11,59 @@ export default function Profile() {
         <View style={styles.cabecalho}>
           <ScreenHeader title="Perfil" />
         </View>
-        <View style={styles.containerColuna}></View>
+        <ScrollView style={{ paddingTop: 40, paddingBottom: 16 }}>
+          <View style={styles.containerColuna}>
+            <View style={styles.containerLinha}>
+              <View style={styles.coluna1}>
+                <Image
+                  source={{
+                    uri: "https://github.com/abnercoolman.png",
+                  }}
+                  alt="Foto perfil do usuario"
+                  style={styles.containerImage}
+                />
+                <TouchableOpacity style={styles.containerLinha1}>
+                  <MaterialCommunityIcons
+                    name="image-edit-outline"
+                    size={24}
+                    color="black"
+                  />
+                  <Text>Mudar foto</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.coluna2}>
+                <Text style={styles.titulo1}>Abner Colman</Text>
+                <Text style={styles.titulo2}>Empresa: Limão com Mel</Text>
+                <Text style={styles.titulo3}>Gerente de RH</Text>
+              </View>
+            </View>
+
+            <TouchableOpacity style={styles.containerLinha1}>
+              <MaterialIcons name="password" size={24} color="black" />
+              <Text style={{ fontFamily: "Inter_500Medium", fontSize: 20 }}>
+                Alterar senha
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.containerLinha1}>
+              <MaterialCommunityIcons
+                name="information-outline"
+                size={24}
+                color="black"
+              />
+              <Text style={{ fontFamily: "Inter_500Medium", fontSize: 20 }}>
+                Sobre o aplicativo
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+
+        <TouchableOpacity style={styles.botaoSair}>
+          <MaterialIcons name="exit-to-app" size={24} color="black" />
+          <Text style={{ fontFamily: "Inter_500Medium", fontSize: 20 }}>
+            Sair do aplicativo
+          </Text>
+        </TouchableOpacity>
       </View>
     </>
   );
--- src/screens/Profile/styles.ts
+++ src/screens/Profile/styles.ts
@@ -19,8 +19,89 @@ export const styles = StyleSheet.create({
   containerColuna: {
     flex: 1,
     flexDirection: "column",
-    paddingHorizontal: 16,
-    width: "auto",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+    paddingHorizontal: 20,
+    width: "100%",
     marginTop: 15,
+  },
+  coluna1: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
+  coluna2: {
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignContent: "flex-start",
+    alignItems: "flex-start",
+    gap: 6,
+  },
+  containerImage: {
+    width: 90,
+    height: 90,
+    borderRadius: 45,
+    justifyContent: "center",
+    backgroundColor: "#FFFFFF",
+  },
+  containerLinha: {
+    flexDirection: "row",
+    gap: 15,
+  },
+  containerLinha1: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    gap: 15,
+
+    borderRadius: 6,
+    borderWidth: 0.5,
+    borderColor: "#F3F4F6",
+    backgroundColor: "#FFF",
+
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 15,
+
+    elevation: 5,
+
+    padding: 8,
+    paddingRight: 16,
+    marginBottom: 12,
+  },
+  titulo1: {
+    fontFamily: "Inter_700Bold",
+    fontSize: 20,
+  },
+  titulo2: {
+    fontFamily: "Inter_600SemiBold",
+    fontSize: 15,
+    color: "#082777",
+  },
+  titulo3: {
+    fontFamily: "Inter_400Regular",
+    fontSize: 13,
+    color: "#082777",
+  },
+  botaoSair: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+
+    paddingHorizontal: 50,
+    paddingVertical: 10,
+    gap: 5,
+    marginBottom: 16,
+    marginHorizontal: 16,
+
+    borderRadius: 12,
+    backgroundColor: "rgba(151, 151, 151, 0.58)",
   },
 });
