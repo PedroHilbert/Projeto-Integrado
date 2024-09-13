1ced24064e94b23450ba33cb29be9fad962c450c
Pedro Hilbert Silva Guerra <pedrohilbertsg@gmail.com>
2024-09-13 16:35:50 -0300

    Update index.tsx

--- src/screens/CadastroFuncionarios/index.tsx
+++ src/screens/CadastroFuncionarios/index.tsx
@@ -1,6 +1,8 @@
+import { Text, View, StyleSheet, Button, ScrollView } from "react-native"; 
+
 import { useState } from "react";
-import { Text, View } from "react-native";
-import { styles } from "./styles";
+
+import { TextInput } from 'react-native-paper';
 
 import ScreenHeader from "../../components/ScreenHeader";
 
@@ -14,11 +16,96 @@ const [categoria, setCategoria] = useState('');
 const [salario, setSalario] = useState('');
 
     return (
-        <>
-        <View style={styles.cabecalho}>
-          <ScreenHeader title="Folha de Ponto" />
-        </View>
+        <View >
+          <View style={styles.cabecalho}> </View>
 
-        </>
+            <Text style={styles.TituloSecao}> ADICIONAR FUNCIONÁRIO </Text>
+
+          <View style={styles.styleInput}>
+            <TextInput
+              mode="outlined"
+              label="Primeiro nome"
+              placeholder="Digite seu Primeiro Nome"
+              onChangeText={setPrimeiroNome}
+              autoCompleteType="name"
+             />
+
+             <TextInput
+              mode="outlined"
+              label="Sobrenome"
+              placeholder="Digite seu SobreNome"
+              onChangeText={setSobreNome}
+              autoCompleteType="nome"
+             />
+            
+            <TextInput
+              mode="outlined"
+              label="Departamento"
+              placeholder="Departamento do seu colaborador"
+              onChangeText={setDepartamento}
+            />
+
+            <TextInput
+              mode="outlined"
+              label="Cargo"
+              placeholder="Digite o Cargo"
+              onChangeText={setCargo}
+            />
+
+            <TextInput
+              mode="outlined"
+              label="Categoria"
+              placeholder="Digite a Categoria"
+              onChangeText={setCategoria}
+            />
+
+            <TextInput
+              mode="outlined"
+              label="Salario"
+              placeholder="Digite o Salario"
+              onChangeText={setSalario}
+              keyboardType="numeric"
+            />
+
+          </View>
+
+          <View> 
+
+            <Button 
+              title="Confirmar"
+            />
+
+          </View>
+        </View>
     );
-};
+}
+
+const styles = StyleSheet.create({
+      cabecalho: {
+        paddingTop: 80,
+        paddingBottom: 20,
+        paddingHorizontal: 32,
+        backgroundColor: "#082777",
+        flexDirection: "row",
+        gap: 6,
+        justifyContent: "space-between",
+        alignItems: "center",
+        alignContent: "center",
+      },
+
+      TituloSecao: {
+        fontSize: 18,
+        fontFamily: "Poppins",
+        paddingTop: 20,
+        fontWeight: 'bold',
+        color: "#071C50",
+      },
+
+      styleInput: {
+        justifyContent: 'center',
+        padding: 20,
+  
+      },
+
+}
+);
